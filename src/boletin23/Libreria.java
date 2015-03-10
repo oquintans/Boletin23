ad29b28c908df8f84d8c4622cc025b58a5db3661
Oscar <Oscar@Demon-PC>
2015-03-10 15:20:22 +0100

    metodoConsultar

--- src/boletin23/Libreria.java
+++ src/boletin23/Libreria.java
@@ -21,21 +21,26 @@ public class Libreria {
     Scanner sc;
     String nombF = "Libreria.txt";
     String delim = ",";
+    String linea;
+    int bucle;
 
     public void add() {
+
         try {
-            ArrayList<Libro> libro = new ArrayList();
-            fich = new PrintWriter(new FileWriter(new File(nombF), true));
-            sc = new Scanner(new File(nombF));
+            do {
+                ArrayList<Libro> libro = new ArrayList();
+                fich = new PrintWriter(new FileWriter(new File(nombF), true));
+                sc = new Scanner(new File(nombF));
+                String n = JOptionPane.showInputDialog("Nombre?");
+                String a = JOptionPane.showInputDialog("Autor?");
+                float p = Float.parseFloat(JOptionPane.showInputDialog("Precio?"));
+                int u = Integer.parseInt(JOptionPane.showInputDialog("Unidades"));
 
-            String n = JOptionPane.showInputDialog("Nombre?");
-            String a = JOptionPane.showInputDialog("Autor?");
-            float p = Float.parseFloat(JOptionPane.showInputDialog("Precio?"));
-            int u = Integer.parseInt(JOptionPane.showInputDialog("Unidades"));
-
-            Libro l = new Libro(n, a, p, u);
-            libro.add(l);
-            fich.println(libro);
+                Libro l = new Libro(n, a, p, u);
+                libro.add(l);
+                fich.println(l);
+                bucle = JOptionPane.showConfirmDialog(null, "Añadir mas?");
+            } while (bucle == 0);
 
         } catch (IOException ex) {
             Logger.getLogger(Libreria.class.getName()).log(Level.SEVERE, null, ex);
@@ -45,25 +50,47 @@ public class Libreria {
     }
 
     public void consultar() {
-        float precio;
+        String nombreL;
+        float precio = 0;
+        String n = "";
         try {
-            ArrayList<Libro> libro = new ArrayList();
-            sc = new Scanner(new File(nombF)).useDelimiter(delim);
-            String preg = JOptionPane.showInputDialog("Introducir Título:");
-            while (sc.hasNextLine()) {
+            do {
+                int aux = 0;
+                ArrayList<Libro> libro = new ArrayList();
+                sc = new Scanner(new File(nombF)).useDelimiter(delim);
+                while (sc.hasNextLine()) {
+                    linea = sc.nextLine();
+                    String[] l = linea.split(",");
+                    for (int i = 0; i < l.length; i += 4) {
+                        libro.add(new Libro(l[i], l[i + 1], Float.parseFloat(l[i + 2]), Integer.parseInt(l[i + 3])));
+                    }
+                }
+                nombreL = JOptionPane.showInputDialog("Nombre del Libro");
+                for (Libro l : libro) {
+                    if (l.getNombre().replace(' ', '-').equalsIgnoreCase(nombreL.replace(' ', '-'))) {
+                        precio = l.getPrecio();
+                        n = l.getNombre();
+                        aux = 1;
 
-            }
+                    }
+                }
+                if (aux == 1) {
+                    JOptionPane.showMessageDialog(null, n + " - Precio: " + precio + "€");
+                } else {
+                    JOptionPane.showMessageDialog(null, "No existe el libro: " + nombreL + ", en la Base de Datos.");
+                }
+                bucle = JOptionPane.showConfirmDialog(null, "Buscar mas?");
+            } while (bucle == 0);
 
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Libreria.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-            fich.close();
+            sc.close();
         }
     }
 
     public void visualizar() {
         ArrayList<Libro> libro = new ArrayList();
-        String linea;
         try {
             sc = new Scanner(new File(nombF)).useDelimiter(delim);
             while (sc.hasNextLine()) {
@@ -74,9 +101,8 @@ public class Libreria {
                 }
             }
             for (int j = 0; j < libro.size(); j++) {
-                System.out.println("Libro " + (j + 1) + "--->" + "Nombre: " + libro.get(j).getNombre() + " Autor: " + libro.get(j).getAutor() + " Precio: " + libro.get(j).getPrecio() + "€" + " Cant: " + libro.get(j).getUnidades());
+                System.out.println("Libro " + (j + 1) + "---> " + "Nombre: " + libro.get(j).getNombre() + " Autor: " + libro.get(j).getAutor() + " Precio: " + libro.get(j).getPrecio() + "€" + " Cant: " + libro.get(j).getUnidades());
             }
-
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Libreria.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
